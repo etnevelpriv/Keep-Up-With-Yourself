a003a4d4f0f9042c3d8a7ba6f8ece704be240948
etnevelpriv <leventerastovits@gmail.com>
2026-04-22 11:25:41 +0200

    mukodo alap regisztracios folyamat keszen van, lehet fiokot csinalni, a szerver oldalon is beallitottam a requirementseket a jelszohoz

--- src/scripts/register.ts
+++ src/scripts/register.ts
@@ -1,5 +1,7 @@
 import "../styles/auth.css";
 import { User } from "./classes/User";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 const init = function () {
     console.log("Betoltodott a register.ts")
     const form: HTMLElement = document.getElementById("registerForm") as HTMLElement;
@@ -13,11 +15,22 @@ const sendRegisterForm = function (e: Event) {
     const email = document.getElementById("emailInput") as HTMLFormElement;
     const password = document.getElementById("passwordInput") as HTMLFormElement;
 
-    const user = new User(name.value, password.value, email.value, new Date(), false);
-    console.log(user);
+    const newUser = new User(name.value, password.value, email.value, new Date(), false);
+    console.log(newUser);
 
     const form = document.getElementById("registerForm") as HTMLFormElement
     form.reset()
+
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, newUser.email, newUser.password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            throw new Error(`Error message: ${error.message}, Error code: ${error.code}`);
+        });
 };
 
 document.addEventListener("DOMContentLoaded", init);
