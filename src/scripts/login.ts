daf041aa1fb4851a8030b54d8457a97f88407465
etnevelpriv <leventerastovits@gmail.com>
2026-04-23 09:49:03 +0200

    na mostmar jonak kell lennie a google authnak

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -1,25 +1,56 @@
 import "../styles/auth.css";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from "firebase/auth";
+import { User } from "./classes/User";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
 
 const init = function () {
     console.log("Betoltodott a register.ts")
     const form: HTMLElement = document.getElementById("loginForm") as HTMLElement;
     form.addEventListener("submit", sendLoginForm);
 
+    // document.getElementById("googleButton")?.addEventListener("click", () => {
+    //     console.log("Google gombra kattintva")
+    //     const provider = new GoogleAuthProvider();
+    //     const auth = getAuth();
+    //     auth.useDeviceLanguage;
+
+    //     signInWithPopup(auth, provider)
+    //         .then((result) => {
+    //             const credential = GoogleAuthProvider.credentialFromResult(result);
+    //             const token = credential.accessToken;
+    //             const user = result.user;
+    //             console.log(credential, result);
+    //         }).catch((error) => {
+    //             throw new Error(`Hiba uzener: ${error.code}, Hiba kod: ${error.errorMessage}, Hitelesito adat: ${GoogleAuthProvider.credentialFromError(error)}`);
+    //         });
+    // });
+
     document.getElementById("googleButton")?.addEventListener("click", () => {
         console.log("Google gombra kattintva")
         const provider = new GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+        provider.addScope('https://www.googleapis.com/auth/userinfo.profile');
         const auth = getAuth();
         auth.useDeviceLanguage;
 
         signInWithPopup(auth, provider)
-            .then((result) => {
+            .then(async (result) => {
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
                 const user = result.user;
                 console.log(credential, result);
+                const name = user.displayName;
+                const email = user.email;
+
+                const docRef = doc(db, "users", user.uid);
+                const docSnap = await getDoc(docRef);
+                if (!(docSnap.exists())) {
+                    const userObj = new User(name, undefined, email, new Date(), true);
+                    userObj.saveUserInfoToDb(user.uid, undefined);
+                };
             }).catch((error) => {
-                throw new Error(`Hiba uzener: ${error.code}, Hiba kod: ${error.errorMessage}, Hitelesito adat: ${GoogleAuthProvider.credentialFromError(error)}`);
+                throw new Error(`Hiba uzener: ${error.code}, Hiba kod: ${error.errorMessage}, Email: ${error.costumData.email}, Hitelesito adat: ${GoogleAuthProvider.credentialFromError(error)}`);
             });
     });
 };
