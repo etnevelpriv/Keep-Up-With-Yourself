c79358ab0c0275a11c2f842ad730bccd48391552
etnevelpriv <leventerastovits@gmail.com>
2026-05-11 14:06:49 +0200

    teljesen kesz a navbar stilusa

--- src/scripts/landing.ts
+++ src/scripts/landing.ts
@@ -1,2 +1,10 @@
 import "../styles/base.css";
 import "../styles/landing.css";
+
+document.getElementById('navToggle')?.addEventListener('click', () => {
+    const hamburger = document.getElementById('navToggle');
+    hamburger?.classList.toggle('active')
+
+    const navList = document.getElementById('navList');
+    navList?.classList.toggle('is-open')
+});
