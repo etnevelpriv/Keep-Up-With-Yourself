ae5284798287d75576a1957cc6d1ed4d8b1ef94b
etnevelpriv <leventerastovits@gmail.com>
2026-05-19 11:32:58 +0200

    tasktype mentes az adatbazisba mukodik

--- src/scripts/create.ts
+++ src/scripts/create.ts
@@ -13,7 +13,7 @@ const init = async function () {
     createSelectOptions(taskTypesNames);
     document.getElementById("createForm")?.addEventListener("submit", (e) => {
         e.preventDefault();
-        const formElements = getFormElements();
+        const formElements = getFormElements(user);
         console.log(formElements);
         const newTask = new Task(formElements[0], formElements[1], new Date(formElements[2]), Number(formElements[3]), formElements[4], "Folyamatban", null, new Date(), new Date())
         console.log(newTask);
@@ -32,7 +32,7 @@ const init = async function () {
     });
 };
 
-const getFormElements = function () {
+const getFormElements = function (user: any) {
     const taskName = (document.getElementById('taskNameInput') as HTMLFormElement).value;
     const taskDesc = (document.getElementById('taskDescTextarea') as HTMLFormElement).value;
     const taskDeadline = (document.getElementById('taskDeadlineInput') as HTMLFormElement).value;
@@ -41,6 +41,7 @@ const getFormElements = function () {
     let taskTypeName = null
     if (taskNewType) {
         taskTypeName = (document.getElementById('taskTypeNameInput') as HTMLFormElement).value;
+        saveTaskTypeToDB(taskTypeName, user)
     } else {
         taskTypeName = (document.getElementById('taskTypeNameSelect') as HTMLFormElement).value;
     }
@@ -87,7 +88,32 @@ const getUser = async function (auth: any) {
     })
 };
 
-const createTaskInDB = async function(task: Task, user:any) {
+const saveTaskTypeToDB = async function (taskType: string, user: any) {
+    if (taskType !== 'Takarítás' && taskType !== 'Munka' && taskType !== 'Tanulás') {
+        const payload = {
+            taskTypeName: taskType,
+            taspType_isSystem: false
+        }
+        // Tudtam, hogy elobb atkell tenni setbe, majd vissza, de en nem igy csinaltam volna, a chatbarat ezt ajanlotta es jol mukodik, szoval itt hagyom
+        user.taskTypes = [...new Set([...user.taskTypes, payload])];
+
+        try {
+            const auth = getAuth();
+            const currentUser = auth.currentUser;
+            if (currentUser) {
+                const userDocRef = doc(db, "users", currentUser.uid);
+                await updateDoc(userDocRef, {
+                    taskTypes: user.taskTypes
+                });
+                console.log("Minden szupi");
+            }
+        } catch (err: any) {
+            throw new Error(err);
+        }
+    }
+}
+
+const createTaskInDB = async function (task: Task, user: any) {
     const taskPayload = {
         taskName: task.taskName,
         taskDesc: task.taskDesc,
