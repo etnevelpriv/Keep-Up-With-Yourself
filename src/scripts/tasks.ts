e13c2ce21a99262d90d32c9c20060acbe60e890b
etnevelpriv <leventerastovits@gmail.com>
2026-05-19 14:17:20 +0200

    alap ts kesz van, user informacioi megvannak, korabbi fuggvenyt felhasznaltam

--- src/scripts/tasks.ts
+++ src/scripts/tasks.ts
@@ -6,7 +6,37 @@ import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "./firebase.ts"
 import { Task } from "./classes/Task.ts";
 
-const init = function () {
-
+const init = async function () {
+    const auth = getAuth();
+    const user = await getUser(auth);
+    const tasks = getTasks(user)
+    console.log(tasks)
 };
+
+const getUser = async function (auth: any) {
+    return new Promise((resolve, reject) => {
+        try {
+            onAuthStateChanged(auth, async (user) => {
+                if (user) {
+                    const docRef = doc(db, "users", user.uid);
+                    const docSnap = await getDoc(docRef);
+                    if (docSnap.exists()) {
+                        if (user.emailVerified) {
+                            resolve(docSnap.data());
+                        };
+                    };
+                };
+            });
+        } catch (err: any) {
+            reject(err);
+            throw new Error(err);
+        }
+    })
+};
+
+const getTasks = function (user) {
+    const tasks = user.tasks;
+    return tasks;
+};
+
 document.addEventListener("DOMContentLoaded", init);
