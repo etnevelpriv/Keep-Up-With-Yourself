5817148b78f856489e2e0668b1a0d0cee0956e6d
etnevelpriv <leventerastovits@gmail.com>
2026-05-19 09:20:40 +0200

    formelemeket mar tudjuk olvasni

--- src/scripts/create.ts
+++ src/scripts/create.ts
@@ -2,11 +2,21 @@ import "../styles/base.css";
 import "./header.ts";
 
 const init = function () {
-    console.log('HELLO')
+    document.getElementById("createForm")?.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const formElements = getFormElements();
+        console.log(formElements);
+
+    });
+};
+
+const getFormElements = function () {
+    const taskName = (document.getElementById('taskNameInput') as HTMLFormElement).value;
+    const taskDesc = (document.getElementById('taskNameInput') as HTMLFormElement).value;
+    const taskDeadline = (document.getElementById('taskDeadlineInput') as HTMLFormElement).value;
+    const taskImportance = (document.getElementById('taskImportanceInput') as HTMLFormElement).value;
+    const taskTypeName = (document.getElementById('taskTypeNameInput') as HTMLFormElement).value;
+    return [taskName, taskDesc, taskDeadline, taskImportance, taskTypeName]
 };
 
 document.addEventListener("DOMContentLoaded", init);
-document.getElementById("createForm")?.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    console.log("bibibi");
-});
