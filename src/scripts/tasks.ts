b70dc53f0aa23365781acc1e7bc11872650c1e7d
etnevelpriv <leventerastovits@gmail.com>
2026-05-19 15:33:58 +0200

    arrbol taskok kesz

--- src/scripts/tasks.ts
+++ src/scripts/tasks.ts
@@ -9,7 +9,9 @@ import { Task } from "./classes/Task.ts";
 const init = async function () {
     const auth = getAuth();
     const user = await getUser(auth);
-    const tasks = getTasks(user)
+    const arr = getTasks(user)
+    const tasks:Task[] = turnArrIntoTasks(arr);
+    createTaskCardsInDOM(tasks);
     console.log(tasks)
 };
 
@@ -34,9 +36,54 @@ const getUser = async function (auth: any) {
     })
 };
 
-const getTasks = function (user) {
+const getTasks = function (user: any) {
     const tasks = user.tasks;
     return tasks;
+};
+
+const turnArrIntoTasks = function (arr: []) {
+    const arrOfTasks: Task[] = [];
+    arr.forEach((element: object) => {
+        console.log(element.TaskCreatedAt)
+        if (element.taskCompletedAt == null) {
+            arrOfTasks.push(new Task(element.taskName, element.taskDesc, new Date(element.taskDeadline.seconds * 1000), element.taskImportance, element.taskTypeName, element.taskStatus, null, new Date(element.TaskCreatedAt.seconds * 1000), new Date(element.taskUpdatedAt.seconds * 1000)))
+        } else {
+            arrOfTasks.push(new Task(element.taskName, element.taskDesc, new Date(element.taskDeadline.seconds * 1000), element.taskImportance, element.taskTypeName, element.taskStatus, new Date(element.taskCompletedAt.seconds * 1000), new Date(element.TaskCreatedAt.seconds * 1000), new Date(element.taskUpdatedAt.seconds * 1000)))
+        }
+    });
+    return arrOfTasks;
+};
+
+const createTaskCardsInDOM = function (tasks: Task[]) {
+    const container = document.getElementById("tasksCards");
+    let i = -1;
+    tasks.forEach((task: Task) => {
+        i++;
+        const card = document.createElement("div");
+        card.classList.add("task-card");
+        card.id = `${i}`;
+
+        const name = document.createElement('h2');
+        const desc = document.createElement('p');
+        const deadline = document.createElement('strong');
+        const importance = document.createElement('span')
+        const type = document.createElement('p');
+
+        name.classList.add("card-name")
+        desc.classList.add("card-desc")
+        deadline.classList.add("card-deadline")
+        importance.classList.add("card-importance")
+        type.classList.add("card-type")
+
+        name.textContent = task.taskName;
+
+        card.appendChild(name)
+        card.appendChild(desc)
+        card.appendChild(deadline)
+        card.appendChild(importance)
+        card.appendChild(type)
+        container?.appendChild(card);
+    });
 };
 
 document.addEventListener("DOMContentLoaded", init);
