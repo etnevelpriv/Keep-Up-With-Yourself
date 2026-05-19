06b3bd220ff511b164eb678b96f45ffba026b7bc
etnevelpriv <leventerastovits@gmail.com>
2026-05-19 09:31:06 +0200

    a user-t mar sikeresen eltudjuk menteni egy valtozoban

--- src/scripts/create.ts
+++ src/scripts/create.ts
@@ -1,7 +1,14 @@
 import "../styles/base.css";
 import "./header.ts";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "./firebase.ts"
 
 const init = function () {
+    const auth = getAuth();
+    const user = getUser(auth);
+
+    // loadTaskTypes()
     document.getElementById("createForm")?.addEventListener("submit", (e) => {
         e.preventDefault();
         const formElements = getFormElements();
@@ -17,6 +24,24 @@ const getFormElements = function () {
     const taskImportance = (document.getElementById('taskImportanceInput') as HTMLFormElement).value;
     const taskTypeName = (document.getElementById('taskTypeNameInput') as HTMLFormElement).value;
     return [taskName, taskDesc, taskDeadline, taskImportance, taskTypeName]
+};
+
+const loadTaskTypes = function () {
+
+}
+
+const getUser = function (auth: any) {
+    onAuthStateChanged(auth, async (user) => {
+        if (user) {
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                if (user.emailVerified) {
+                  return(docSnap.data());
+                };
+            };
+        };
+    });
 };
 
 document.addEventListener("DOMContentLoaded", init);
