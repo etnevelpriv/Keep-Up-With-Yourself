329b47c8e704ed27759cd7c7ceadbad18c25ec6f
etnevelpriv <leventerastovits@gmail.com>
2026-05-26 21:39:33 +0200

    auth validator kesz. itt nem kell tobb teszt szerintem, hisz ez csak frontend validacio

--- src/tests/auth/auth.validators.test.ts
+++ src/tests/auth/auth.validators.test.ts
@@ -3,7 +3,32 @@ import { validateRegisterInput, validateLoginInput } from '../../services/auth/a
 
 describe("VALID Auth Validator unit teszt", () => {
     test("VALID register input", async () => {
-        expect(()=>validateRegisterInput("TestUser", "teszt@google.com", "Password123!", new Date(), false)).not.toThrow();
+        expect(() => validateRegisterInput("TestUser", "teszt@google.com", "Password123!", new Date(), false)).not.toThrow();
+    });
+    test("VALID login input", async () => {
+        expect(() => validateLoginInput("teszt@google.com", "Password123!")).not.toThrow();
     });
 });
-
+describe("INVALID Auth Validator unit teszt", () => {
+    test("INVALID register input invalid email", async () => {
+        expect(() => validateRegisterInput("TestUser", "nemjoemail", "Password123!", new Date(), false)).toThrow();
+    });
+    test("INVALID login input invalid email", async () => {
+        expect(() => validateLoginInput("tesztgoogle.com", "Password123!")).toThrow();
+    });
+    test("INVALID register input invalid password", async () => {
+        expect(() => validateRegisterInput("TestUser", "teszt@google.com", "Password123fdasfefasd fdsafa sdfasfdfsadf !", new Date(), false)).toThrow();
+    });
+    test("INVALID login input invalid password", async () => {
+        expect(() => validateLoginInput("teszt@google.com", "Password123")).toThrow();
+    });
+    test("INVALID register input invalid name", async () => {
+        expect(() => validateRegisterInput("", "teszt@google.com", "Password123!", new Date(), false)).toThrow();
+    });
+    test("INVALID register input invalid datum", async () => {
+        expect(() => validateRegisterInput("TestUser", "teszt@google.com", "Password123!", new Date(2999, 12, 12), false)).toThrow();
+    });
+    test("INVALID register input invalid verified", async () => {
+        expect(() => validateRegisterInput("TestUser", "teszt@google.com", "Password123!", new Date(2999, 12, 12), "nem bool" as any)).toThrow();
+    });
+});
