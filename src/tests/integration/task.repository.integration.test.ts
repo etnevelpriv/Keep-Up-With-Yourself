732498ad53741d9b9416c34fd5f2e23e6b15e436
etnevelpriv <leventerastovits@gmail.com>
2026-05-26 18:29:39 +0200

    deleteTask teszt is mukodik

--- src/tests/integration/task.repository.integration.test.ts
+++ src/tests/integration/task.repository.integration.test.ts
@@ -1,7 +1,7 @@
 import { expect, test, describe } from 'vitest'
 import createTestUser from '../modelsTests/userTestSetup'
 import createTestTask from '../modelsTests/taskTestSetup';
-import { createTask, getTask, updateTask } from '../../repositories/task.repository';
+import { createTask, deleteTask, getTask, updateTask } from '../../repositories/task.repository';
 import { getAuthenticatedDb } from './setup/userTestDb';
 import { afterAllSetup, beforeAllSetup, beforeEachSetup } from './setup/firebaseTestSetup';
 beforeAllSetup()
@@ -18,11 +18,11 @@ describe("Task Repository (service klon) Integration teszt", () => {
         const tid = await createTask(authenticatedDb as any, uid, task);
         const dbTask = await getTask(authenticatedDb as any, uid, tid);
 
-        expect(dbTask).not.toBe(false);
+        expect(dbTask).not.toBe(null);
         expect((dbTask as any).id).toBe(tid);
         expect((dbTask as any).taskName).toBe(task.taskName);
     });
-    test("Task dokumentum lerehozasa es modostasa es lekerese", async () => {
+    test("Task dokumentum lerehozasa es modositasa es lekerese", async () => {
         const user = createTestUser();
         const task = createTestTask();
         const email = "taskmodositasteszt@gmail.com";
@@ -36,8 +36,21 @@ describe("Task Repository (service klon) Integration teszt", () => {
         });
         const dbTask = await getTask(authenticatedDb as any, uid, tid);
 
-        expect(dbTask).not.toBe(false);
+        expect(dbTask).not.toBe(null);
         expect((dbTask as any).taskStatus).toBe("Teljesített");
         expect((dbTask as any).taskName).toBe(task.taskName);
+    });
+    test("Task dokumentum lerehozasa es torlese", async () => {
+        const user = createTestUser();
+        const task = createTestTask();
+        const email = "tasktorlesteszt@gmail.com";
+        const uid = `${crypto.randomUUID()}`
+        const authenticatedDb = getAuthenticatedDb(uid, email, user.verified);
+        const tid = await createTask(authenticatedDb as any, uid, task);
+        const dbTask = await getTask(authenticatedDb as any, uid, tid);
+        expect(dbTask).not.toBe(null);
+        await deleteTask(authenticatedDb as any, uid, tid);
+        const falsedbTask = await getTask(authenticatedDb as any, uid, tid);
+        expect(falsedbTask).toBe(null);
     });
 });
