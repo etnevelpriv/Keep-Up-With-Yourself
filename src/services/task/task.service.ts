6e48d736b0d14a8563d6fde57377878ab9488972
etnevelpriv <leventerastovits@gmail.com>
2026-05-27 16:09:06 +0200

    task service refaktor

--- src/services/task/task.service.ts
+++ src/services/task/task.service.ts
@@ -1,16 +1,14 @@
 import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
-import { db } from "../../scripts/firebase.ts";
+import type { Firestore } from "firebase/firestore";
 
-const getTasksCollectionRef = function (uid: string) {
+const getTasksCollectionRef = function (db: Firestore, uid: string) {
     return collection(db, "users", uid, "tasks");
 };
-
-const getTaskDocumentRef = function (uid: string, tid: string) {
+const getTaskDocumentRef = function (db:Firestore, uid: string, tid: string) {
     return doc(db, "users", uid, "tasks", tid);
 };
-
-export const createTask = async function (uid: string, task: any) {
-    const taskDocRef = await addDoc(getTasksCollectionRef(uid), {
+export const createTask = async function (db:Firestore, uid: string, task: any) {
+    const taskDocRef = await addDoc(getTasksCollectionRef(db, uid), {
         taskName: task.taskName,
         taskDesc: task.taskDesc,
         taskDeadline: task.taskDeadline,
@@ -21,36 +19,29 @@ export const createTask = async function (uid: string, task: any) {
         taskCreatedAt: task.taskCreatedAt,
         taskUpdatedAt: task.taskUpdatedAt
     });
-    console.log("Task hozzáadva");
     return taskDocRef.id;
 };
-
-export const getTasks = async function (uid: string) {
-    const snapshot = await getDocs(getTasksCollectionRef(uid));
+export const getTasks = async function (db:Firestore,uid: string) {
+    const snapshot = await getDocs(getTasksCollectionRef(db, uid));
     return snapshot.docs.map((taskDoc) => ({
         id: taskDoc.id,
         ...taskDoc.data()
     }));
 };
-
-export const getTask = async function (uid: string, tid: string) {
-    const taskSnap = await getDoc(getTaskDocumentRef(uid, tid));
-
+export const getTask = async function (db:Firestore, uid: string, tid: string) {
+    const taskSnap = await getDoc(getTaskDocumentRef(db, uid, tid));
     if (!taskSnap.exists()) {
         return null;
     }
-
     return {
         id: taskSnap.id,
         ...taskSnap.data()
     };
 };
-
-export const updateTask = async function (uid: string, tid: string, data: any) {
-    await updateDoc(getTaskDocumentRef(uid, tid), data);
+export const updateTask = async function (db:Firestore, uid: string, tid: string, data: any) {
+    await updateDoc(getTaskDocumentRef(db, uid, tid), data);
 };
-
-export const deleteTask = async function (uid: string, tid: string) {
-    await deleteDoc(getTaskDocumentRef(uid, tid));
+export const deleteTask = async function (db:Firestore, uid: string, tid: string) {
+    await deleteDoc(getTaskDocumentRef(db, uid, tid));
 };
 
