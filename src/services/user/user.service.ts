944000e41f55b3870065300bf8468c77f06a1e25
etnevelpriv <leventerastovits@gmail.com>
2026-05-27 16:04:10 +0200

    teljes user service refaktor

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,53 +1,32 @@
 import { doc, getDoc, updateDoc, setDoc, deleteDoc } from "firebase/firestore";
-import { db } from "../../scripts/firebase.ts"
-import { showErrorPopUp } from "../../utils/popup.ts"
+import type { Firestore } from "firebase/firestore";
 
-export const syncUserVerificationStatus = async function (uid: string) {
+export const syncUserVerificationStatus = async function (db: Firestore, uid: string) {
     await updateDoc(doc(db, "users", uid), {
         userVerified: true
     });
 };
-
-export const createUserDocumentInDatabase = async function (uid: string, email: string, name: string, createdAt: Date, verified: boolean) {
-    try {
-        const defaultTaskTypes = ["Tanulás","Munka","Takarítás"];
-
-        await setDoc(doc(db, "users", uid), {
-            userID: uid,
-            userEmail: email,
-            userName: name,
-            userCreatedAt: createdAt,
-            userVerified: verified,
-            taskTypes: defaultTaskTypes
-        });
-        console.log("Uj dokumentum letrehozva az adatbazisban");
-    } catch (err: any) {
-        showErrorPopUp("Hiba történt az adatbázisba való mentés folyamán.");
-        throw new Error(err)
-    };
+export const createUserDocumentInDatabase = async function (db: Firestore, uid: string, email: string, name: string, createdAt: Date, verified: boolean) {
+    const defaultTaskTypes = ["Tanulás", "Munka", "Takarítás"];
+    await setDoc(doc(db, "users", uid), {
+        userID: uid,
+        userEmail: email,
+        userName: name,
+        userCreatedAt: createdAt,
+        userVerified: verified,
+        taskTypes: defaultTaskTypes
+    });
 };
-export const updateUserDocumentInDatabase = async function (uid: string, data: any) {
-    try {
-        const userDocRef = doc(db, "users", uid);
-        await updateDoc(userDocRef, data);
-        console.log("Az adatbázisban történő módosítás sikeres volt");
-
-    } catch (err: any) {
-        throw new Error(err);
-    }
-
+export const updateUserDocumentInDatabase = async function (db: Firestore, uid: string, data: any) {
+    await updateDoc(doc(db, "users", uid), data);
 };
-export const getUserDocumentFromDatabase = async function (uid: string) {
-    const docRef = doc(db, "users", uid);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-        console.log("A felhasznalo letezik az adatbazisban:", docSnap.data());
-        return (docSnap.data());
-    } else {
-        console.log("Nem letezik ehhez a felhasznalohoz dokumentum az adatbazisban.");
+export const getUserDocumentFromDatabase = async function (db: Firestore, uid: string) {
+    const docSnap = await getDoc(doc(db, "users", uid));
+    if (!docSnap.exists()) {
         return false;
-    };
+    }
+    return (docSnap.data());
 };
-export const deleteUserDocumentFromDatabase = async function (uid: string) {
+export const deleteUserDocumentFromDatabase = async function (db:Firestore, uid: string) {
     await deleteDoc(doc(db, "users", uid));
 };
