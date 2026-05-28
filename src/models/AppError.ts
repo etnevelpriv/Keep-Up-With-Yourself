3a020d5187f635b768b47166ddbd90887655c7ab
etnevelpriv <leventerastovits@gmail.com>
2026-05-28 10:13:49 +0200

    AI segitett javitani a classomon, igy mostmar oroklodik az eredeti error objektumbol

--- src/models/AppError.ts
+++ src/models/AppError.ts
@@ -1,12 +1,13 @@
-import type { AppErrorInterface } from "../interfaces/AppErrorInterface";
+import type { AppErrorInterface } from "../interfaces/AppErrorInterface.ts";
 
-export class AppError implements AppErrorInterface {
+export class AppError extends Error implements AppErrorInterface {
     code:string;
     message:string;
     constructor (code:string, message:string) {
+        super(message);
         this.code = code;
         this.message = message;
-        
+        Object.setPrototypeOf(this, AppError.prototype);
     };
     getMessage () {
         return this.message;
