8a078239d3b1cece45cf39fd4e2730c828338cfd
etnevelpriv <leventerastovits@gmail.com>
2026-05-30 13:03:01 +0200

    a getErrorMessages mostmar a firebase errort is kezeli, nem csak az apperrort

--- src/utils/errors/getErrorMessage.ts
+++ src/utils/errors/getErrorMessage.ts
@@ -1,13 +1,10 @@
+import { FirebaseError } from "firebase/app";
 import { AppError } from "../../models/AppError.ts";
 import { errorMessages } from "./errorMessages.ts";
 
-export const getErrorMessage = function (error:unknown) {
-    if (error instanceof AppError) {
-        if (errorMessages[error.code]) {
-            return errorMessages[error.code];
-        } else {
-            return errorMessages["unknown"];
-        };
+export const getErrorMessage = function (error: unknown) {
+    if ((error instanceof AppError || error instanceof FirebaseError) && (errorMessages[error.code])) {
+        return errorMessages[error.code];
     };
     return errorMessages["unknown"];
-}
+};
