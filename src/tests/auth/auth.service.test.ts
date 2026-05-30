ef2644ab3145fac679d798dc8570b31ef1d8f0eb
etnevelpriv <leventerastovits@gmail.com>
2026-05-30 14:28:29 +0200

    auth service teszt elso alap fuggvenye kesz

--- src/tests/auth/auth.service.test.ts
+++ src/tests/auth/auth.service.test.ts
@@ -1,19 +1,33 @@
 import { expect, test, describe, vi } from 'vitest';
-import { createUserWithEmailAndPassword, type Auth } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, type UserCredential } from "firebase/auth";
+import { createUserDocumentInDatabase } from '../../services/user/user.service';
+import { registerWithEmail } from '../../services/auth/auth.service';
+import type { Firestore } from "firebase/firestore";
+
 vi.mock("firebase/auth", () => ({
-    createUserWithEmailAndPassword: vi.fn()
+    createUserWithEmailAndPassword: vi.fn(),
+    sendEmailVerification: vi.fn(),
+    getAuth: vi.fn(() => {return {}})
+}));
+vi.mock("../../services/user/user.service.ts", () => ({
+    createUserDocumentInDatabase: vi.fn()
 }));
 
-describe("Auth Service Mock Teszt", () => {
-    test("firebase register mock", async () => {
+describe("VALID Auth Service Mock Teszt", () => {
+    test("VALID registerWith email teszt", async () => {
+        const db: Firestore = {} as Firestore;
         vi.mocked(createUserWithEmailAndPassword).mockResolvedValue({
             user: {
-                uid: "TEST_UID"
+                uid: "TESZT_UID"
             }
-        } as any);
-        const userCredential = await createUserWithEmailAndPassword({} as Auth, "test@gmail.com", "Jelszo123!");
-        const user = userCredential.user;
-        expect(user.uid).toBe("TEST_UID");
+        }as UserCredential);
+        vi.mocked(createUserDocumentInDatabase).mockResolvedValue(undefined);
+        vi.mocked(sendEmailVerification).mockResolvedValue(undefined);
+        await registerWithEmail(db, "TesztUser", "teszt@gmail.hu", "Jelszo123!", new Date(), false);
+        
+        expect(createUserWithEmailAndPassword).toHaveBeenCalled();
+        expect(createUserDocumentInDatabase).toHaveBeenCalled();
+        expect(sendEmailVerification).toHaveBeenCalled();
     });
 });
 
