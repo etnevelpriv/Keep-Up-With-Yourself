05b9d609b47e3f09734a87e28ca06dc72fd1d27b
etnevelpriv <leventerastovits@gmail.com>
2026-06-04 16:49:42 +0200

    valid createUserDocumentInDatabase kesz

--- src/tests/service/user/user.service.test.ts
+++ src/tests/service/user/user.service.test.ts
@@ -13,8 +13,21 @@ beforeEach(() => {
     vi.resetAllMocks();
 });
 describe("VALID User Service Mock teszt", () => {
-
-});
-describe("INVALID User Service Mock teszt", () => {
-
+    test("VALID createUserDocumentInDatabase teszt", async () => {
+        const db: Firestore = {} as Firestore;
+        const createdAt = new Date()
+        vi.mocked(doc).mockReturnValue({
+            uid: "TESZT_UID"
+        } as any);
+        await createUserDocumentInDatabase(db, "TESZT_UID", "teszt@gmail.com", "NEV", createdAt, false);
+        expect(doc).toHaveBeenCalledWith(db, "users", "TESZT_UID");
+        expect(setDoc).toHaveBeenCalledWith({ uid: "TESZT_UID" }, {
+            userID: "TESZT_UID",
+            userEmail: "teszt@gmail.com",
+            userName: "NEV",
+            userCreatedAt: createdAt,
+            userVerified: false,
+            taskTypes: ["Tanulás", "Munka", "Takarítás"]
+        });
+    });
 });
