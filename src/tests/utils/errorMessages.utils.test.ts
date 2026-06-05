d08de4165584ca332c2acc957ee2d03e794975f6
etnevelpriv <leventerastovits@gmail.com>
2026-06-05 19:57:20 +0200

    model tesztek atmozgatasa az uj unit test mappaba

--- src/tests/utils/errorMessages.utils.test.ts
+++
@@ -1,5 +0,0 @@
-import { describe, test, expect } from "vitest";
-import { FirebaseError } from "firebase/app";
-import { AppError } from "../../models/AppError";
-import { getErrorMessage } from "../../utils/errors/getErrorMessage";
-import { errorMessages } from "../../utils/errors/errorMessages";
