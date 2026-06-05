df87bf33303489fc4a0bd1aafb61724478b19c4c
etnevelpriv <leventerastovits@gmail.com>
2026-06-05 20:46:09 +0200

    Az elobb elirtam, nem popup, hanem handleUiError. Keszen van a teljes tesztfajl

--- src/tests/unit/utils/handleUiError.utils.test.ts
+++ src/tests/unit/utils/handleUiError.utils.test.ts
@@ -2,17 +2,30 @@ import { describe, test, expect, vi, beforeEach } from "vitest";
 import { AppError } from "../../../models/AppError";
 import { handleUiError } from "../../../utils/errors/handleUiError";
 import { showErrorPopUp } from "../../../utils/popup";
+import { FirebaseError } from "firebase/app";
 
 vi.mock("../../../utils/popup", () => ({
     showErrorPopUp: vi.fn()
 }));
 
-describe("Popup Unit Teszt", () => {
-    beforeEach(()=>{
+describe("handleUiError Unit Teszt", () => {
+    beforeEach(() => {
         vi.clearAllMocks();
     });
-    test("Apperror eseteln megfelelo hiba jelenik meg", ()=>{
+    test("AppError eseten megfelelo hiba jelenik meg", () => {
         handleUiError(new AppError("validation/invalid-email"));
         expect(showErrorPopUp).toHaveBeenCalledWith("Az e-mail cím nincs megfelelően megadva.");
+    });
+    test("FirebaseError eseten megfelelo hiba jelenik meg", () => {
+        handleUiError(new FirebaseError("auth/email-already-in-use", "Eredeti firebase error message"));
+        expect(showErrorPopUp).toHaveBeenCalledWith("Ezzel az e-mail címmel már létezik felhasználói fiók.");
+    });
+    test("Ismeretlen FirebaseError eseten megfelelo hiba jelenik meg", () => {
+        handleUiError(new FirebaseError("nincsilyen/hibakod", "Eredeti firebase error message"));
+        expect(showErrorPopUp).toHaveBeenCalledWith("Ismeretlen hiba történt.");
+    });
+    test("Ismeretlen Error eseten megfelelo hiba jelenik meg", () => {
+        handleUiError(new Error("ismeretlenHiba"));
+        expect(showErrorPopUp).toHaveBeenCalledWith("Ismeretlen hiba történt.");
     });
 });
